7fc901dfe8d5f1a35b2b4092ce6635b388b75aee
tostis005 <tostis005@gmail.com>
2026-09-02 23:03:48 +0200

    Remove legacy generic page label

--- page.php
+++ page.php
@@ -2,9 +2,8 @@
 
 <?php while ( have_posts() ) : the_post(); ?>
 	<div class="article-shell">
-		<?php food_breadcrumbs(); ?>
+		<?php function_exists( 'food_language_breadcrumbs' ) ? food_language_breadcrumbs() : food_breadcrumbs(); ?>
 		<header class="article-header">
-			<div class="post-category">FOOD</div>
 			<h1><?php the_title(); ?></h1>
 			<?php if ( has_excerpt() ) : ?><p class="article-deck"><?php echo esc_html( get_the_excerpt() ); ?></p><?php endif; ?>
 		</header>
